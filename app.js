af3e8dd02633a07ab9f7670b6823823c96f1189d
pullaposki <pullaposki@windowslive.com>
2024-01-02 19:10:23 +0200

    Add new project files and improve game logic
    This commit adds several project-specific configuration files to the project. Moreover, the game logic in `app.js` has been revised and enhanced. A complete 52-card deck has been implemented, and drawing cards will now decrease the deck. Winning and losing conditions have been refined, and the game can now handle 'Ace' cards (worth either 1 or 11 points) correctly.

--- app.js
+++ app.js
@@ -1,34 +1,76 @@
-﻿
-const drawButton = document.getElementById("draw-button");
+﻿const drawButton = document.getElementById("draw-button");
 const sumParagraph = document.getElementById("sum-paragraph");
 
-let cards = [];
 let sum = 0;
 
-console.log(sum);
+const cardRanks = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'];
+const cardValues = [[1, 11], 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
+const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
 
-function generateRandomCard() {
-    return Math.floor(Math.random() * 14) + 1;
+let deck = createDeck();
+
+function createDeck() {
+    let deck = [];
+    for (let i = 0; i < cardRanks.length; i++) {
+        for (let suit of suits) {
+            deck.push({ 
+                card: cardRanks[i], 
+                suit: suit, 
+                value: cardValues[i] });
+        }
+    }
+    return deck;
 }
 
-function drawNewCard(currentCards){
-    if(sum>0) return generateRandomCard();
-    
-    let newCard = generateRandomCard();
-    while(currentCards.includes(newCard)) {
-        newCard = generateRandomCard();
+drawButton.addEventListener("click", ()=> {
+    handleClick();
+});
+
+function handleClick(){
+    if (deck.length === 0){
+        alert("No more cards left in the deck");
+        return;
     }
 
-    return newCard
+    const newCard = drawFromDeck();
+    sum += countCards(newCard);
+    displaySum();
+
+    if(sum === 21) {
+        alert("you won: "+sum);
+        reset();
+    }
+    else if(sum > 21) {
+        alert("you lost: "+ sum);
+        reset();
+    }
+}
+
+function drawFromDeck() {
+    const randomIndex = Math.floor(Math.random() * deck.length);
+    const card = deck[randomIndex];                      
+    removeFromDeck(randomIndex);
+    return card;
+}
+
+function removeFromDeck(cardIndex){
+    deck.splice(cardIndex, 1);
+}
+
+function countCards(card) {
+    if (card.card === 'Ace' && sum <= 10) return 11;
+    if (Array.isArray(card.value)) {
+        return card.value[0];
+    } else {
+        return card.value;
+    }
 }
 
 function displaySum(){
     sumParagraph.textContent = sum.toString();
 }
 
-drawButton.addEventListener("click", ()=> {
-    const newCard = drawNewCard(cards);
-    cards.push(newCard);
-    sum += newCard;
-    displaySum();    
-})
+function reset() {
+    sum = 0;
+    displaySum();
+}
