f2f0016b0ec691b13d1d52bb063eabbb50c0fb6a
pullaposki <pullaposki@windowslive.com>
2024-01-03 18:48:58 +0200

    Refactor game logic and enhance UI interactions
    In this commit, the blackjack game logic was significantly refactored in the app.js file to increase maintainability and readability. New constants were introduced to manage animation duration and game resets. The HTML file was also updated to relocate the 'new game' button. Furthermore, enhanced the user interface with animations and fade-ins. The New Game button now appears with a fade-in effect after a game ends, giving a more polished feedback to the user.

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-﻿const drawButton = document.getElementById("draw-button");
+﻿const ANIMATION_DURATION = 350;
+const NEW_GAME_FADE_IN_DURATION = 2000;
+
+const drawButton = document.getElementById("draw-button");
 const newGameButton = document.getElementById("new-game-button");
 const sumContainer = document.getElementById("sum-paragraph");
 const cardContainer = document.getElementById("card-container");
 const messageParagraph = document.getElementById("message-paragraph");
 
-let sum = 0;
+newGameButton.addEventListener("click", resetGame);
+drawButton.addEventListener("click", handleDrawClick);
 
+let sum = 0;
 const cardRanks = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'];
 const cardValues = [[1, 11], 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
 const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
 
-let deck = createDeck();
-
 hideCard();
+hideNewGameButton();
 
+let deck = createDeck();
 function createDeck() {
     let deck = [];
     for (let i = 0; i < cardRanks.length; i++) {
@@ -27,57 +32,60 @@ function createDeck() {
     return deck;
 }
 
-drawButton.addEventListener("click", ()=> {
-    handleClick();
-});
-
-function handleClick(){
-    if (deck.length === 0){
+function handleDrawClick(){
+    if (isDeckEmpty()){
         messageParagraph.textContent = "No more cards left in the deck";
         return;
     }
 
+    doNewCardOperations();
+    checkWinningConditions();
+}
+
+function isDeckEmpty(){
+    return deck.length === 0;
+}
+
+function doNewCardOperations(){
     const newCard = drawFromDeck();
     sum += countCards(newCard);
     displaySum();
     displayCard(newCard);
+}
 
+function checkWinningConditions(){
     if(sum === 21) {
-        updateMessage("you won", true);
+        handleGameOverMessage("you won", true);
+        showNewGameButton();
         disableDrawButton();
     }
     else if(sum > 21) {
-        updateMessage("you lost", false);
+        handleGameOverMessage("you lost", false);
+        showNewGameButton();
         disableDrawButton();
     }
 }
 
-function updateMessage(text, isWinner) {
-    // Remove the animation class
+function handleGameOverMessage(text, isWinner) {
+    setTimeout(()=>{
+        removeAnimationClasses();
+        void messageParagraph.offsetWidth;
+        messageParagraph.textContent = text;
+        const color = determineColor(isWinner);
+        messageParagraph.classList.add(color);
+        messageParagraph.classList.add('zoom-in');
+    }, ANIMATION_DURATION)
+}
+
+function removeAnimationClasses(){
     messageParagraph.classList.remove('zoom-in');
     messageParagraph.classList.remove('red');
     messageParagraph.classList.remove('green');
-
-    // Trigger reflow to restart animation
-    void messageParagraph.offsetWidth;
-
-    // Update text
-    messageParagraph.textContent = text;
-
-    // Determine the color based on win/loss
-    if(isWinner){
-        messageParagraph.classList.add('green');
-    } else {
-        messageParagraph.classList.add('red');
-    }
-
-    // Re-add animation class
-    messageParagraph.classList.add('zoom-in');
 }
 
-newGameButton.addEventListener("click", ()=>{
-    reset();
-})
+function determineColor(isWinner){
+    return isWinner ? 'green' : 'red';
+}
 
 function displayCard(card) {
     const {card: name, suit} = card;
@@ -114,9 +122,23 @@ function countCards(card) {
 }
 
 function displaySum() {
-    sumContainer.textContent = sum.toString();
-    sumContainer.classList.add('lerp');
-    setTimeout(() => { sumContainer.classList.remove('lerp'); }, 300);
+    setTimeout(()=>{
+        sumContainer.textContent = sum.toString();
+        sumContainer.classList.add('lerp');
+        setTimeout(() => { sumContainer.classList.remove('lerp'); }, ANIMATION_DURATION);
+    }, ANIMATION_DURATION)
+    
+}
+
+function hideNewGameButton(){
+    newGameButton.style.display = "none";
+}
+
+function showNewGameButton() {
+    setTimeout(()=>{
+        newGameButton.style.display = "block";
+        newGameButton.classList.add('fade-in');    
+    }, NEW_GAME_FADE_IN_DURATION)
 }
 
 function disableDrawButton(){
@@ -131,7 +153,8 @@ function enableDrawButton(){
     drawButton.style.cursor = "";
 }
 
-function reset() {
+function resetGame() {
+    hideNewGameButton();
     hideCard();
     messageParagraph.textContent="";
     sum = 0;
